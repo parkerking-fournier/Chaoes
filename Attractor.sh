60227aedc68bc1cd88fc944be29f0ef301e63407
parkerking-fournier <parker.k.fournier@gmail.com>
2018-11-18 10:34:52 -0500

    First Commit

---
+++ Attractor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+start='0.5000'
+end='0.5002'
+step='0.00001'
+LENGTH=10
+
+for i in `seq 0 $LENGTH`;
+ do
+         VAR='-f king_fournier -s -i 38500 -t 0.002 -o '$start' -e '$end' -w 0.00002 attractors/finished/attractor_frame_ply_files/frame_'$i'.ply'
+         ./attractors/src/attractors $VAR
+         
+         VAR=' attractors/src/ply2obj.py attractors/finished/attractor_frame_ply_files/frame_'$i'.ply attractors/finished/attractor_frame_obj_files/frame_'$i'.obj'
+         python $VAR 
+        
+         start=`echo "$start + $step" | bc`
+         end=`echo "$end + $step" | bc`
+ done
